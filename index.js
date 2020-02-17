aa7cce5e147d69ebbb1bf553677bf13ceed717fb
emanuelecoppola1 <therealmanu@icloud.com>
2020-02-17 12:31:20 +0100

    aggiunto ejs

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require ("express");
 const path = require ("path");
 const app = express ();
+app.set ("views",path.join(__dirname, "views"));
+app.set("view engine","ejs");
 app.listen (3000);
+const todos=[
+{ id: 1,text:"boh",done:false},
+{ id: 6,text:"b5h",done:false},
+{ id: 9,text:"b",done:true}
+];
+
 
 app.get("/",function (req, res) { 
-  res.send("che vuo tu");
+  res.render("index", {todos:todos});
 } );
-app.get("/er",function (req, res) { 
-  res.send("vernazzaro mio zio");
-} );
+
 app.use(function (req, res) { 
   res.sendFile(path.join(__dirname, "public", "404.html"));
 } );
