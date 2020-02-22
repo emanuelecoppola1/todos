2d343aa7868f782c4cfa6afb861bea8bcad7088d
emanuelecoppola1 <therealmanu@icloud.com>
2020-02-22 12:53:23 +0000

    lettura dati da db

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const express = require ("express");
 const path = require ("path");
+const sqlite3= require ("sqlite3");
+const bodyParser=require("body-parser");
+
 const app = express ();
 app.set ("views",path.join(__dirname, "views"));
 app.set("view engine","ejs");
-app.listen (3000);
-const todos=[
-{ id: 1,text:"boh",done:false},
-{ id: 6,text:"b5h",done:false},
-{ id: 9,text:"b",done:true}
-];
-
+app.use(bodyParser.urlencoded({
+extended:true}));
+const db= new sqlite3.Database("todos.db",function(){
+	app.listen (3000);
+})
 
 app.get("/",function (req, res) { 
-  res.render("index", {todos:todos});
+ const sql="select * from todos;";
+ db.all(sql,function(err,rows){
+ res.render("index", {todos:rows});
+})
+ 
 } );
+
+
+app.post("/newtodo",function(req,res){
+console.log(req.body);
+})
+
 
 app.use(function (req, res) { 
   res.sendFile(path.join(__dirname, "public", "404.html"));
